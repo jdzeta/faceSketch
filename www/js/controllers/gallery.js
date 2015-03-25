ddb53cc9ce62a0fe5bb198efbfc19c8556449466
jdzeta <jdzeta@gmail.com>
2015-03-25 17:56:11 +0100

    commit gallery

--- www/js/controllers/gallery.js
+++ www/js/controllers/gallery.js
@@ -3,40 +3,37 @@
   .module('FaceSketch')
   .controller('GalleryCtrl', function ($cordovaCamera)
   {
-  	this.getPhoto = function(source) {
-	// Retrieve image file location from specified source
-		navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-		destinationType: destinationType.FILE_URI,
-		sourceType: source });
-	}
 
-	this.images = [];
+  	var self = this;
 
-	this.getPictures = function(){
-		this.images = Camera.PictureSourceType.PHOTOLIBRARY;
-		alert(this.images);
-	};
+  	self.test = "TEST";
 
-	this.testGetPics = function(){
-		navigator.camera.getPicture(this.onCaptureSuccess, this.onCaptureFail, {
+  	self.image = {
+  		"src" : "",
+  		"alt" : "Photo 2 draw selected from library"
+  	}
+
+	self.testGetPics = function(){
+		navigator.camera.getPicture(self.onCaptureSuccess, self.onCaptureFail, {
 			allowEdit: true,
 			correctOrientation: true,
-			destinationType: Camera.DestinationType.FILE_URI,
-			sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+			destinationType: Camera.DestinationType.DATA_URL,
+			sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
 		});
 	}
 
-	this.onCaptureSuccess = function(imageData) {
-		alert(imageData);
+	self.onCaptureSuccess = function(imageData) {
+		//alert(imageData);
+		self.image.src = "data:image/jpeg;base64," + imageData;
 	}
 
-	this.onCaptureFail = function(message) {
+	self.onCaptureFail = function(message) {
 	    alert('Failed because: ' + message);
 	}
 
 	// file://storage/sdcard0/Android/data/com.faceplusplus.facesketch/cache/##.jpg
 
-	this.testGetPics();
+	self.testGetPics();
 
   });
 })();

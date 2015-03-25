4420e1db4d1e4cb151cac926512694e8a5b5e53c
hugohil <hilairehugo@gmail.com>
2015-03-25 18:17:02 +0100

    get landmark from picture

--- www/js/services/facepp.js
+++ www/js/services/facepp.js
@@ -7,11 +7,38 @@
 
       return {
         getLandmark: function(img){
-            imgur
-              .upload(img, true)
-              .then(function (data){
-                console.log(data);
-              });
+          // Post image on imgur
+          imgur
+            .upload(img, true)
+            .then(function (model){
+              if(!model.link.match(/^http:\/\/i.imgur.com\//)){
+                return null;
+              }
+              // Get face id from imgur link
+              $http
+                .get('https://apius.faceplusplus.com/v2/detection/detect?url=' + model.link + '&api_secret=' + auth.fpp_api_secret + '&api_key=' + auth.fpp_api_key)
+                .success(function (detect){
+                  console.log(detect);
+                  // Check if facepp has detected a face
+                  if(detect.face.length){
+                    // Get landmark from face id
+                    $http
+                      .get('http://apius.faceplusplus.com/detection/landmark?api_secret=' + auth.fpp_api_secret + '&api_key=' + auth.fpp_api_key + '&face_id=' + detect.face[0].face_id + '&type=83p')
+                      .success(function (landmark){
+                        console.log(landmark);
+                        return landmark.result[0].landmark;
+                      })
+                      .error(function (err){
+                        console.log(err);
+                        return null;
+                      });
+                  }
+                })
+                .error(function (err){
+                  console.log(err);
+                  return null;
+                });
+            });
         }
       }
     })

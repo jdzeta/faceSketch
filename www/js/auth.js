62da17895bad47047bd66a5c6e8b85fe61c13882
jdzeta <jdzeta@gmail.com>
2015-03-26 18:51:39 +0100

    add auth.js

---
+++ www/js/auth.js
@@ -0,0 +1,9 @@
+(function(){
+  angular
+  .module('FaceSketch')
+  .constant('auth', {
+    fpp_api_key: '59dd998ce39b17a8639240612db99ab5',
+    fpp_api_secret: '95GuHHPOf6f_M4o92_bjcrU6fEfyJuxh',
+    imgur_api_key: 'Client-ID 28bcc8050df696a'
+  })
+})();

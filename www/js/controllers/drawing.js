3e9e8efb7ec9b4cb05c1473bcc3b8332cb19f42d
jdzeta <jdzeta@gmail.com>
2015-03-27 10:46:32 +0100

    Adding file saving

--- www/js/controllers/drawing.js
+++ www/js/controllers/drawing.js
@@ -4,5 +4,15 @@
     .controller('DrawingCtrl', function (sketch){
       var self = this;
       sketch.drawFace();
+
+      window.canvas2ImagePlugin.saveImageDataToLibrary(
+      	function(msg){
+      		console.log(msg);
+      	},
+      	function(err){
+      		console.log(err);
+      	},
+      	document.getElementById('myCanvas')
+      );
     });
 })();
